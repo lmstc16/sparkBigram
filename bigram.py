04e9ed70577f95901cf00ca598814bb9fdcddbd7
lmstc16 <lmstc16@gmail.com>
2016-12-01 15:51:17 -0800

    modify py

--- bigram.py
+++ bigram.py
@@ -3,18 +3,31 @@ from operator import add
 import string
 
 input_file = "file:///usr/local/spark/input.txt"
-output_file = "file:///usr/local/spark/output.txt"
+output_file = "file:///usr/local/spark/output"
 
 conf = SparkConf().setMaster('local').setAppName('BigramCount')
 sc = SparkContext(conf = conf)
 
 RDDvar = sc.textFile(input_file)
 
-sentences = RDDvar.flatMap(lambda line: line.split("."))
-words = sentences.flatMap(lambda line: line.split(" "))
-bigrams = words.flatMap(lambda xs: zip(xs, xs[1:]))
+bigrams = RDDvar.flatMap(lambda line: line.split(".")) \
+                .map(lambda sentences: sentences.split(" ")) \
+                .flatMap(lambda word: zip(word, word[1:])) \
+                .map(lambda bigram: (bigram, 1)) \
+                .reduceByKey(add) \
+                .sortBy(lambda x: x[1], False)
 
-result = bigrams.map(lambda bigram: (bigram, 1))
-aggreg1 = result.reduceByKey(add)
+most_common = bigrams.first()[0]
 
-result.saveAsTextFile(output_file)
+mylen = bigrams.count()
+
+agg = bigrams.map(lambda a: (0, a[1]))
+top = agg.reduce(lambda a, b: (a[0] + 1, a[1] + b[1]) if a[1] + b[1] < mylen / 10 else a)
+
+file = open("/usr/local/spark/result.txt", "w")
+file.write("the total number of bigrams: %d \n" %(mylen))
+file.write("the most common bigram: %s \n" %(most_common))
+file.write("the number of bigrams required to add up to 10 percent of all bigrams: %d" %(top[0]))
+file.close()
+
+bigrams.saveAsTextFile(output_file)
--- go.sh
+++ go.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
 # Build Docker images
-docker build -t myspark:2.0 ./
+docker build -t sparkimg:2.0 ./
 
 # start the container
-docker run -it -p 8088:8088 -p 8042:8042 -p 4040:4040 -h sandbox myspark:2.0 bash
-
+#docker run -it -p 8088:8088 -p 8042:8042 -p 4040:4040 -h sandbox sparkimg:2.0 --name myspark bash
+docker run -d --name myspark sparkimg:2.0
+sleep 10
 # run the code inside the container
-cd /usr/local/spark
-./bin/spark-submit bigram.py 
+#cd /usr/local/spark/
+echo "start run python doc"
+docker exec myspark /bin/spark-submit /usr/local/spark/bigram.py
+
+#echo the result
+docker exec myspark cat /usr/local/spark/output/part-00000
+docker exec myspark cat /usr/local/spark/result.txt
+
+#Clean
+docker stop myspark
+docker rm myspark
